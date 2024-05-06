44a0a41b5477b59e1688a5c1ffe1031cf7a12c43
Rafael Rodriguez <rafael.nc@me.com>
2024-05-05 20:51:19 -0400

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Rafael Rodriguez on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
